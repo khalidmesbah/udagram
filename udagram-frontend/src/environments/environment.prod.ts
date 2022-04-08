ae786b4e03b4c1cac299ed386cd9c30b304c0870
khalidMesbah <swekhalidmesbah6@gmail.com>
2022-04-08 22:22:29 +0200

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://udagram-api-dev.eba-iyqaty6j.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://udagram-api-dev.eba-22k2p32w.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
